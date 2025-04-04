c0aabc3ff84c85e580fc9debe7e250894c2c269d
01huan <ethan20080205@gmail.com>
2025-04-04 23:59:53 +0800

    sec commit

--- script.js
+++ script.js
@@ -1,22 +1,88 @@
 window.onload = function() {
-    document.querySelector("textarea").focus();
+    document.getElementById('code').focus();
 };
-
-function convert() {
-    equation.innerHTML = "$$" + document.querySelector("textarea").value + "$$";
-    MathJax.typesetClear([document.getElementById("equation")]);
+function LaTeX() {
+    equation.innerHTML = "$$" + document.getElementById("code").value + "$$"; MathJax.typesetClear([document.getElementById("equation")]);
     MathJax.typesetPromise([document.getElementById("equation")]);
 }
 
-function insert(str) {
-    const textarea = document.querySelector("textarea");
-    const start = textarea.selectionStart;
-    const end = textarea.selectionEnd;
-    const pos = textarea.scrollTop;
-    textarea.value = textarea.value.substring(0,start) + str 
-    + textarea.value.substring(end, textarea.value.length);
+function Limit() {
+    let word = "\\lim_{{}\\to{}}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
     textarea.focus();
-    textarea.selectionStart = start + str.length;
-    textarea.selectionEnd = start + str.length;
-    textarea.scrollTop = pos;
+}
+function Fraction() {
+    let word = "\\dfrac{}{}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function Integral() {
+    let word = "\\int_{}^{}{}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function Binomial() {
+    let word = "\\binom{}{}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function Summation() {
+    let word = "\\sum_{{}={}}^{}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function Matrix() {
+    let word = "\\begin{bmatrix}\n\t{} & {} & {}\\\\\n\t{} & {} & {}\\\\\n\t{} & {} & {}\n\\end{bmatrix}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function LinearSystem() {
+    let word = "\\begin{cases}\n\t{}\\\\\n\t{}\n\\end{cases}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
+}
+function nthRoot() {
+    let word = "\\sqrt[]{}";
+    let textarea = document.getElementById("code");
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    textarea.setRangeText(word, start, end, "end");
+    textarea.selectionStart = start;
+    textarea.selectionEnd = start + word.length;
+    textarea.focus();
 }
